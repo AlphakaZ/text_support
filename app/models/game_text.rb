0c2ca11c2e0589495e0378d0946790a29531e3fd
kazuto <kazuto@ubuntu.(none)>
2013-04-20 17:16:27 +0900

    シーン個別ページに所有テキストビューを追加

--- app/models/game_text.rb
+++ app/models/game_text.rb
@@ -1,3 +1,4 @@
 class GameText < ActiveRecord::Base
   attr_accessible :background, :characterFace, :fontSize, :music, :orderNum, :part, :scene_id, :talker
+  belongs_to :scene
 end
--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,3 +1,4 @@
 class Scene < ActiveRecord::Base
   attr_accessible :explanation, :game_id, :person, :tag1, :tag2, :tag3, :title, :when, :where
+  has_many :game_texts
 end
